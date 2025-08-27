1d76fc806678f9a8c983c9b4cd7d12a9d96ec49b
DayPilot Code <code@daypilot.org>
2025-08-27 18:08:02 +0000

    Published

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './App.css';
+import ReactScheduler from "./scheduler/ReactScheduler.jsx";
+
+const App = () => {
+  return (
+    <ReactScheduler />
+  );
+}
+
+export default App;
